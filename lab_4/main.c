3643a5bfcbed9bbda2b55508e652a9f951752db8
b1ad3runn3r <31962327+b1ad3runn3r@users.noreply.github.com>
2022-12-03 10:33:58 +0300

    lab_4: change according to task
    The task was to wrap output into double quotes and add time profiling

--- lab_4/main.c
+++ lab_4/main.c
@@ -1,9 +1,13 @@
+#include <time.h>
+
 #include "lib/string.h"
 
 int main() {
     char* input = NULL;
+    clock_t timer;
     
     while(1) {
+        timer = clock();
         input = my_readline("Enter a line: ");
 
         if (!input) {
@@ -12,7 +16,11 @@ int main() {
 
         input = remove_spaces(input);
         swap(&input);
-        printf("%s\n", input);
+        printf("\"%s\"\n", input);
+
+        timer = clock() - timer;
+        double time_taken = (double)timer / CLOCKS_PER_SEC;
+        printf("Took %lf seconds!\n", time_taken);
 
         free(input);
     }
--- lab_4/main_readline.c
+++ lab_4/main_readline.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <readline/readline.h>
 
@@ -52,8 +53,10 @@ void swap(char** str) {
 
 int main() {
     char* input = NULL;
+    clock_t timer;
 
     while(1) {
+        timer = clock();
         input = readline("Enter a line: ");
         
         if (!input) {
@@ -62,7 +65,11 @@ int main() {
 
         input = remove_spaces(input);
         swap(&input);
-        printf("%s\n", input);
+        printf("\"%s\"\n", input);
+
+        timer = clock() - timer;
+        double time_taken = (double)timer / CLOCKS_PER_SEC;
+        printf("Took %lf seconds!\n", time_taken);
         
         free(input);
     }
